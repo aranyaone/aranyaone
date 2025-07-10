c9eb8a44932b357400a21edfbaa0dacb1c86c876
MAKAM SRINIVAS <srinivasmakam357@gmail.com>
2025-07-10 22:31:36 +0530

    Create Navbar.js

---
+++ components/Navbar.js
@@ -0,0 +1,9 @@
+const Navbar = () => {
+  return (
+    <nav style={{ padding: '1rem', backgroundColor: '#f5f5f5', borderBottom: '1px solid #ddd' }}>
+      <h3>👑 Aranya Empire</h3>
+    </nav>
+  );
+};
+
+export default Navbar;

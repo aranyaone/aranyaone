926c9491b187750085d85661f5f5fab7651b3c78
MAKAM SRINIVAS <srinivasmakam357@gmail.com>
2025-07-10 21:20:47 +0530

    Create layout.js

---
+++ app/services/layout.js
@@ -0,0 +1,8 @@
+export default function ServicesLayout({ children }) {
+  return (
+    <div style={{ padding: 20, background: "#e3f2fd" }}>
+      <h2>💼 Our Services</h2>
+      {children}
+    </div>
+  );
+}

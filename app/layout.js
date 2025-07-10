b39dba29183782b0618905addc465683c27b8f0d
MAKAM SRINIVAS <srinivasmakam357@gmail.com>
2025-07-10 08:09:24 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,39 +1,10 @@
-export const metadata = {
-  title: "AranyaOne – The AI Empire",
-  description: "The World's First AI-Powered Business Empire by Srinivas Makam",
-  openGraph: {
-    title: "Welcome to AranyaOne.com 👑",
-    description: "Join the world's first AI-Powered Business Empire, built by Srinivas Makam.",
-    url: "https://www.aranyaone.com",
-    siteName: "Aranya One",
-    images: [
-      {
-        url: "https://aranyaone.vercel.app/aranya-logo.png", // update if needed
-        width: 800,
-        height: 600,
-      },
-    ],
-    locale: "en_IN",
-    type: "website",
-  },
-};
-
-import { Geist, Geist_Mono } from 'next/font/google';
-import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="msvalidate.01" content="F2AD1B41FC838077051C56AC2ABB90B8" />
+        <title>AranyaOne – AI Empire</title>
+      </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
       </body>

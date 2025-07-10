ee5f710d624533acc0ca2915dad133eebef10fcb
MAKAM SRINIVAS <srinivasmakam357@gmail.com>
2025-07-10 22:58:25 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,13 +1,12 @@
 import Navbar from '../components/Navbar';
-// app/layout.js
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head />
-      <body style={{ fontFamily: "sans-serif", margin: 6, padding: 6 }}>
-  <Navbar />
-  {children}
-</body>
+      <body>
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }

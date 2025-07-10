791d7dd04861676b48edbeaf2c3a8fd7112624c4
MAKAM SRINIVAS <srinivasmakam357@gmail.com>
2025-07-10 21:18:15 +0530

    Create layout.js

---
+++ app/founder/layout.js
@@ -0,0 +1,8 @@
+export default function FounderLayout({ children }) {
+  return (
+    <div style={{ padding: 20, background: "#fff8e1" }}>
+      <h2>👑 Our Founder</h2>
+      {children}
+    </div>
+  );
+}
